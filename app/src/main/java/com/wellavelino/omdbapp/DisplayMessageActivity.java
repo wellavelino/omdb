9e9c9c21eb144ee5de2b702bb175488e6aef41c4
Wellington Avelino <wellington.avelino@outlook.com>
2018-06-14 11:50:38 -0300

    Simple Android app to remember how intents works

---
+++ app/src/main/java/com/wellavelino/omdbapp/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.wellavelino.omdbapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textVew = findViewById(R.id.textView);
+        textVew.setText(message);
+    }
+}
